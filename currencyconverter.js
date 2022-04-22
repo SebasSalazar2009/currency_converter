d44b7a3fe5138d009a1604999ad53b9d7ea0a450
Sebastian Salazar <sebastiansalazar@Sebastians-MacBook-Air.local>
2022-04-22 12:06:47 -0400

    conversion rate

---
+++ currencyconverter.js
@@ -0,0 +1,47 @@
+const usd = 1;
+const pounds = 0.78;
+
+function convertCurrency(currency, conversionRate) {
+    const curreturn = currency * conversionRate;
+    return curreturn;
+}
+
+console.log (convertCurrency(usd,pounds));
+const result = convertCurrency(usd,pounds)
+console.log(`${usd.toFixed(2)} in usd is ${result.toFixed(2)} pounds`)
+
+
+
+
+
+
+
+
+// console.log('subtotal:      ', subtotal.toFixed(2));
+// console.log('tax:           ', myTax.toFixed(2));
+// console.log('-----------');
+// console.log('Total:     ', myTotal.toFixed(2));
+
+
+// function calculateTax(subtotal, taxRate) {
+//     const tax = subtotal * taxRate;
+//     return tax;
+//}
+// function calculateTotal(subtotal, tax) {
+//     return subtotal + tax; 
+// }
+// const myTax = calculateTax(subtotal, taxRate);
+
+
+
+// const myTotal = calculateTotal(subtotal, myTax);
+
+
+
+
+// console.log('subtotal:      ', subtotal.toFixed(2));
+// console.log('tax:           ', myTax.toFixed(2));
+// console.log('-----------');
+// console.log('Total:     ', myTotal.toFixed(2));
+// © 2022 GitHub, Inc.
+// Terms
